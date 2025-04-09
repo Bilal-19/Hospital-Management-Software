d60af44ce994aa9ef4abb04b862d2f662c98e8fe
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-04-09 14:27:38 +0500

    Created 'receptionist' table schema.

---
+++ database/migrations/2025_04_09_092450_create_receptionist_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('receptionist', function (Blueprint $table) {
+            $table->id();
+            $table->string("fullName");
+            $table->string("emailAddress");
+            $table->string("phoneNumber");
+            $table->string("gender");
+            $table->string("shiftTiming");
+            $table->string("assignedDepartment");
+            $table->string("joiningDate");
+            $table->timestamps();
+
+             // Foreign Key
+             $table->unsignedBigInteger('user_id');
+             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('receptionist');
+    }
+};
