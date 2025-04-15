a347489c59a3c5ec40125e0085a91fb7736946d9
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-04-15 14:56:44 +0500

    Updated Admin dashboard.

--- resources/views/Admin/Dashboard.blade.php
+++ resources/views/Admin/Dashboard.blade.php
@@ -1,10 +1,5 @@
 @extends('AdminLayout.main')
 @php
-    $fetchAppoinments = DB::table('appointments')
-        ->where('doctorName', '=', Auth::user()->name)
-        ->limit(3)
-        ->get();
-
     $isAttendanceMarked = DB::table('staff')
         ->whereDate('created_at', today())
         ->where('user_id', '=', Auth::user()->id)
@@ -43,41 +38,21 @@ $countTodayAppointment = DB::table('appointments')
         <div class="w-full my-5 flex flex-col md:flex-row justify-between space-y-5 md:space-y-0">
             <div
                 class="w-80 mx-auto md:mx-0 md:w-1/4 bg-white rounded-md shadow p-6 border-1 border-t border-t-slate-700 flex flex-col items-center justify-center space-x-2">
-                <p class="text-lg font-medium text-gray-800">Today's Appointments</p>
+                <p class="text-lg font-medium text-gray-800">April Patients</p>
                 <p><i class="fa-solid fa-hospital-user"></i> {{ $countTodayAppointment }}</p>
             </div>
 
             <div
                 class="w-80 mx-auto md:mx-0 md:w-1/4 bg-white rounded-md shadow p-6 border-1 border-t border-t-slate-700 flex flex-col items-center justify-center space-x-2">
-                <p class="text-lg font-medium text-gray-800">Upcoming Appointments</p>
+                <p class="text-lg font-medium text-gray-800">Appointments</p>
                 <p> <i class="fa-solid fa-calendar-check"></i> 100</p>
             </div>
 
             <div
                 class="w-80 mx-auto md:mx-0 md:w-1/4 bg-white rounded-md shadow p-6 border-1 border-t border-t-slate-700 flex flex-col items-center justify-center space-x-2">
-                <p class="text-lg font-medium text-gray-800">Diagnosis</p>
-                <p> <i class="fa-solid fa-person-dots-from-line"></i> 100</p>
+                <p class="text-lg font-medium text-gray-800">Revenue</p>
+                <p> <i class="fa-solid fa-dollar-sign"></i> 100</p>
             </div>
-        </div>
-
-        <div class="w-full bg-white rounded shadow p-6 mt-10">
-            <h3 class="text-lg font-semibold mb-4">Upcoming Appoinments</h3>
-            <table class="w-full">
-                <tr class="border-b border-gray-500">
-                    <th class="py-3">Date</th>
-                    <th class="py-3">Time</th>
-                    <th class="py-3">Patient Name</th>
-                    <th class="py-3">Reason for Visit</th>
-                </tr>
-                @foreach ($fetchAppoinments as $record)
-                    <tr class="border-b border-gray-300">
-                        <td class="py-3">{{ date('M d, Y', strtotime($record->appoinmentDate)) }}</td>
-                        <td class="py-3">{{ $record->appoinmentTime }}</td>
-                        <td class="py-3">{{ $record->patientName }}</td>
-                        <td class="py-3">{{ $record->reasonForVisit }}</td>
-                    </tr>
-                @endforeach
-            </table>
         </div>
     </main>
     </div>
