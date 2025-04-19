ee36773e2ba1938ef2ef703d3b495286c73c5a5e
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-04-19 17:24:52 +0500

    Fix shift management layout.

--- resources/views/Admin/ShiftManagement.blade.php
+++ resources/views/Admin/ShiftManagement.blade.php
@@ -1,7 +1,7 @@
 @extends('AdminLayout.main')
 @section('section')
     <!-- Main Content -->
-    <main class="flex-1">
+    <main class="flex-1 p-3 md:p-6 overflow-x-auto">
         <div class="bg-white p-6 rounded shadow">
             <h2 class="text-xl font-semibold mb-10">Assign Shift to Staff</h2>
 
@@ -47,37 +47,34 @@
 
         <div class="bg-white p-6 rounded shadow mt-5">
             <h2 class="text-xl font-semibold mb-5">Employees Shift</h2>
-            <table class="w-80 md:w-full">
-                <tr class="border-b border-gray-500 text-[#6B7280] text-xs">
-                    <th class="font-medium text-start py-3">ID</th>
-                    <th class="font-medium text-start py-3">Staff Name</th>
-                    <th class="font-medium text-start py-3">Start Date</th>
-                    <th class="font-medium text-start py-3">End Date</th>
-                    <th class="font-medium text-start py-3">Applicable Days</th>
-                    <th class="font-medium text-start py-3">Assigned By</th>
-                    <th class="font-medium text-start py-3">Status</th>
-                    <th class="font-medium text-start py-3">Actions</th>
-                </tr>
-                @foreach ($fetchAllStaffShift as $record)
-                    <tr class="border-b border-gray-300 text-sm text-[#111827]">
-                        <td class="py-3">{{ $record->id }}</td>
-                        <td class="py-3">{{ $record->staffName }}</td>
-                        <td class="py-3">
-                            {{ date('d-M-Y', strtotime($record->startDate)) }}
-                        </td>
-                        <td class="py-3">
-                            {{ date('d-M-Y', strtotime($record->endDate)) }}
-                        </td>
-                        <td class="py-3">{{ $record->applicableDays }}</td>
-                        <td class="py-3">{{ $record->assignedBy }}</td>
-                        <td class="py-3">{{ $record->status }}</td>
-                        <td class="py-3">
-                            <a href="">Update</a>
-                            <a href="">Delete</a>
-                        </td>
+            <div class="overflow-x-auto">
+                <table class="min-w-max md:w-full">
+                    <tr class="border-b border-gray-500 text-[#6B7280] text-xs">
+                        <th class="font-medium text-start p-3">ID</th>
+                        <th class="font-medium text-start p-3">Staff Name</th>
+                        <th class="font-medium text-start p-3">Start Date</th>
+                        <th class="font-medium text-start p-3">End Date</th>
+                        <th class="font-medium text-start p-3">Applicable Days</th>
+                        <th class="font-medium text-start p-3">Assigned By</th>
+                        <th class="font-medium text-start p-3">Status</th>
                     </tr>
-                @endforeach
-            </table>
+                    @foreach ($fetchAllStaffShift as $record)
+                        <tr class="border-b border-gray-300 text-sm text-[#111827]">
+                            <td class="p-3">{{ $record->id }}</td>
+                            <td class="p-3">{{ $record->staffName }}</td>
+                            <td class="p-3">
+                                {{ date('d-M-Y', strtotime($record->startDate)) }}
+                            </td>
+                            <td class="p-3">
+                                {{ date('d-M-Y', strtotime($record->endDate)) }}
+                            </td>
+                            <td class="p-3">{{ $record->applicableDays }}</td>
+                            <td class="p-3">{{ $record->assignedBy }}</td>
+                            <td class="p-3">{{ $record->status }}</td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
         </div>
     </main>
     </div>
