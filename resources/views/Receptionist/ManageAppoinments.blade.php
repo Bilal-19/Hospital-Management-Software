73177ad808a1b2f4b3f2fa990a71e7fc45dc7ab5
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-04-21 13:11:50 +0500

    Fixed book appointment layout.

--- resources/views/Receptionist/ManageAppoinments.blade.php
+++ resources/views/Receptionist/ManageAppoinments.blade.php
@@ -2,7 +2,7 @@
 
 @section('section')
     <!-- Main Content -->
-    <main class="flex-1 p-6">
+    <main class="flex-1 p-6 overflow-x-auto">
         <div class="w-80 md:w-full bg-white rounded shadow p-6 mb-10">
             <h3 class="text-lg font-semibold mb-4">Book Appoinment</h3>
 
@@ -94,7 +94,7 @@
         </div>
 
         <div class="w-full bg-white rounded shadow p-6">
-            <div class="flex flex-row justify-between items-center mb-5">
+            <div class="flex flex-col md:flex-row justify-between md:items-center mb-5 space-y-3 md:space-y-0">
                 <div>
                     <h3 class="text-lg font-semibold">Upcoming Appoinments</h3>
                     @if (count($fetchAppoinments) == 0)
@@ -108,31 +108,33 @@
                         class="bg-black text-white px-3 py-2 rounded-md">View All Appointments</a>
                 </div>
             </div>
-            <table class="w-80 md:w-full">
-                <tr class="border-b border-gray-500 text-[#6B7280] text-xs">
-                    <th class="font-medium text-start py-3">Date & Time</th>
-                    <th class="font-medium text-start py-3">Doctor</th>
-                    <th class="font-medium text-start py-3">Department</th>
-                    <th class="font-medium text-start py-3">Patient Name</th>
-                    <th class="font-medium text-start py-3">Reason for Visit</th>
-                    <th class="font-medium text-start py-3">Actions</th>
-                </tr>
-                @foreach ($fetchAppoinments as $record)
-                    <tr class="border-b border-gray-300 text-sm text-[#111827]">
-                        <td class="py-3">{{ date('M d, Y', strtotime($record->appointmentDate)) }}
-                            {{ $record->appointmentTime }}</td>
-                        <td class="py-3">{{ $record->doctorName }}</td>
-                        <td class="py-3">{{ $record->department }}</td>
-                        <td class="py-3">{{ $record->patientName }}</td>
-                        <td class="py-3">{{ $record->reasonForVisit }}</td>
-                        <td class="py-3">
-                            <a href="" class="font-medium text-black mr-3">Reschedule</a>
-                            <a href="{{ route('Receptionist.CancelAppointment', ['id' => $record->id]) }}"
-                                class="font-medium text-[#DC2626]">Cancel</a>
-                        </td>
+            <div class="overflow-x-auto">
+                <table class="min-w-max md:w-full">
+                    <tr class="border-b border-gray-500 text-[#6B7280] text-xs">
+                        <th class="font-medium text-start py-3">Date & Time</th>
+                        <th class="font-medium text-start py-3">Doctor</th>
+                        <th class="font-medium text-start py-3">Department</th>
+                        <th class="font-medium text-start py-3">Patient Name</th>
+                        <th class="font-medium text-start py-3">Reason for Visit</th>
+                        <th class="font-medium text-start py-3">Actions</th>
                     </tr>
-                @endforeach
-            </table>
+                    @foreach ($fetchAppoinments as $record)
+                        <tr class="border-b border-gray-300 text-sm text-[#111827]">
+                            <td class="py-3">{{ date('M d, Y', strtotime($record->appointmentDate)) }}
+                                {{ $record->appointmentTime }}</td>
+                            <td class="py-3">{{ $record->doctorName }}</td>
+                            <td class="py-3">{{ $record->department }}</td>
+                            <td class="py-3">{{ $record->patientName }}</td>
+                            <td class="py-3">{{ $record->reasonForVisit }}</td>
+                            <td class="py-3">
+                                <a href="" class="font-medium text-black mr-3">Reschedule</a>
+                                <a href="{{ route('Receptionist.CancelAppointment', ['id' => $record->id]) }}"
+                                    class="font-medium text-[#DC2626]">Cancel</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
         </div>
     </main>
 @endsection
