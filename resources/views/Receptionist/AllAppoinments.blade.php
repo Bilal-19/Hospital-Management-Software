03e6479ce441130f88bd50bbeb0510ac48cd303f
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-04-21 13:08:39 +0500

    Fixed receptionist all appointments page.

--- resources/views/Receptionist/AllAppoinments.blade.php
+++ resources/views/Receptionist/AllAppoinments.blade.php
@@ -2,36 +2,38 @@
 
 @section('section')
     <!-- Main Content -->
-    <main class="flex-1 p-6">
+    <main class="flex-1 p-6 overflow-x-auto">
         <div class="w-full bg-white rounded shadow p-6">
             <h3 class="text-lg font-semibold">All Appoinments</h3>
             <p class="text-gray-500 text-sm mb-5">{{count($fetchAppoinments)}} records found</p>
-            <table class="w-80 md:w-full">
-                <tr class="border-b border-gray-500 text-[#6B7280] text-xs">
-                    <th class="font-medium text-start py-3">Date & Time</th>
-                    <th class="font-medium text-start py-3">Doctor</th>
-                    <th class="font-medium text-start py-3">Department</th>
-                    <th class="font-medium text-start py-3">Patient Name</th>
-                    <th class="font-medium text-start py-3">Reason for Visit</th>
-                    <th class="font-medium text-start py-3">Actions</th>
-                </tr>
-                @foreach ($fetchAppoinments as $record)
-                    <tr class="border-b border-gray-300 text-sm text-[#111827]">
-                        <td class="py-3">{{ date('M d, Y', strtotime($record->appointmentDate)) }}
-                            {{ $record->appointmentTime }}</td>
-                        <td class="py-3">{{ $record->doctorName }}</td>
-                        <td class="py-3">{{ $record->department }}</td>
-                        <td class="py-3">{{ $record->patientName }}</td>
-                        <td class="py-3">{{ $record->reasonForVisit }}</td>
-                        <td class="py-3">
-                            <button onclick="openRescheduleModal({{ $record->id }})" type="button"
-                                class="font-medium text-black mr-3">Reschedule</button>
-                            <a href="{{ route('Receptionist.CancelAppointment', ['id' => $record->id]) }}"
-                                class="font-medium text-[#DC2626]">Cancel</a>
-                        </td>
+            <div class="overflow-x-auto">
+                <table class="min-w-max md:w-full">
+                    <tr class="border-b border-gray-500 text-[#6B7280] text-xs">
+                        <th class="p-3">Date & Time</th>
+                        <th class="p-3">Doctor</th>
+                        <th class="p-3">Department</th>
+                        <th class="p-3">Patient Name</th>
+                        <th class="p-3">Reason for Visit</th>
+                        <th class="p-3">Actions</th>
                     </tr>
-                @endforeach
-            </table>
+                    @foreach ($fetchAppoinments as $record)
+                        <tr class="border-b border-gray-300 text-sm text-[#111827]">
+                            <td class="p-3">{{ date('M d, Y', strtotime($record->appointmentDate)) }}
+                                {{ $record->appointmentTime }}</td>
+                            <td class="p-3">{{ $record->doctorName }}</td>
+                            <td class="p-3">{{ $record->department }}</td>
+                            <td class="p-3">{{ $record->patientName }}</td>
+                            <td class="p-3">{{ $record->reasonForVisit }}</td>
+                            <td class="p-3">
+                                <button onclick="openRescheduleModal({{ $record->id }})" type="button"
+                                    class="font-medium text-black mr-3">Reschedule</button>
+                                <a href="{{ route('Receptionist.CancelAppointment', ['id' => $record->id]) }}"
+                                    class="font-medium text-[#DC2626]">Cancel</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
 
             <!-- Modal -->
             <div id="rescheduleModal"
