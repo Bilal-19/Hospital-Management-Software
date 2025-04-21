b0d05a4b2b3426fe0ee63dc12e32e03c2f09e195
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-04-21 10:41:16 +0500

    Added feature: Doctor can add and view their profile picture.

--- resources/views/Doctor/MyProfile.blade.php
+++ resources/views/Doctor/MyProfile.blade.php
@@ -5,11 +5,12 @@
     <main class="flex-1 p-6">
         <div class="bg-white p-6 rounded shadow">
             <h2 class="text-xl font-semibold mb-4">My Profile</h2>
-        </div>
+
 
         <form action="{{ route('Doctor.UpdateProfile') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <h4 class="text-lg font-semibold my-4">Personal Information</h4>
+            <img src="{{asset("Doctors/Profile/".$fetchRecord->profilePicture)}}" alt="my-profile" class="size-32 my-5 object-cover rounded-full">
             <div class="w-full grid grid-cols-1 md:grid-cols-3 gap-5 space-y-1 md:space-y-0">
                 <div class="flex flex-col">
                     <label for="fullName">Full Name:</label>
@@ -145,6 +146,7 @@
             </div>
             <button class="bg-black text-white px-3 py-1 rounded-md mt-5">Update Profile</button>
         </form>
+    </div>
     </main>
     </div>
 @endsection
